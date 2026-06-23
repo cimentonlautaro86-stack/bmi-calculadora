aea881416f2179917e5c68c5c6c81c782d1a2cf9
cimentonlautaro86-stack <cimentonlautaro86@gmail.com>
2026-06-23 19:24:47 -0300

    Enhance BMI calculator with condition output
    Added BMI condition output based on calculated IMC.

--- bmi.c
+++ bmi.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
 int main() {
-	
-	float peso;
-	float altura;
-	float imc;
-	
-	printf("Ingrese el peso en kg: ");
-	scanf("%f", &peso);
-	
-	printf("Ingrese la altura en metros: ");
-	scanf("%f", &altura);
-	
-	imc = peso / (altura * altura);
-	
-	printf("\nSu indice de masa corporal es: %.2f\n\n", imc);
-	
-	printf("Indice    |  Condicion\n");
-	printf("-----------------------------\n");
-	printf("<18.5     |  Bajo peso\n");
-	printf("18.5-24.9 |  Normal\n");
-	printf("25-29.9   |  Sobrepeso\n");
-	printf(">=30      |  Obesidad\n");
-	
-	return 0;
+    float peso, altura, imc;
+
+    printf("Ingrese el peso en kg: ");
+    scanf("%f", &peso);
+
+    printf("Ingrese la altura en metros: ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    printf("\nSu indice de masa corporal es: %.2f\n\n", imc);
+
+    printf("    Indice    |  Condicion\n");
+    printf("-----------------------------\n");
+    printf("    <18.5     |  Bajo peso\n");
+    printf(" 18.5 a 24.9  |  Normal\n");
+    printf(" 25.0 a 29.9  |  Sobrepeso\n");
+    printf("     >=30     |  Obesidad\n");
+
+    printf("\nUsted se encuentra en la condicion: ");
+
+    if (imc < 18.5) {
+        printf("Bajo peso\n");
+    }
+    else if (imc < 25.0) {
+        printf("Normal\n");
+    }
+    else if (imc < 30.0) {
+        printf("Sobrepeso\n");
+    }
+    else {
+        printf("Obesidad\n");
+    }
+
+    return 0;
 }
